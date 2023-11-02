6b34cc405a03791c21a916a9dd18b3eb08d2c813
Alisson Rodrigues Fernandes <alissonrodrigues565656@gmail.com>
2023-11-02 20:08:04 -0300

    feat: estilo header

---
+++ components/header/Header.tsx
@@ -0,0 +1,40 @@
+import PreHeader from "$store/islands/header/PreHeader.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+import SearchIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/search.tsx";
+import UserIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/user.tsx";
+import ShoppingCartIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/shopping-cart.tsx";
+import ChevronDownIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/chevron-down.tsx";
+
+export interface Props {
+  logo?: ImageWidget;
+}
+
+export default function Header({logo = "/logo.svg"}: Props) {
+  return (
+    <>
+    <PreHeader/>
+    <header class="lg:container mx-8 my-5 text-xl md:text-base">
+      <div class="flex items-center justify-between">
+        <img
+          class="object-cover w-20"
+          src={logo}
+        />
+        <div class="flex align-center justify-between gap-6">
+          <a href="#">Masculino</a>
+          <a href="#">Feminino</a>
+          <a href="#">Infantil</a>
+          <a class="flex gap-2 items-center" href="#">Categorias <ChevronDownIcon class="w-5 h-5"/></a>
+        </div>
+        <div class="relative">
+          <input placeholder="Procurar produto" class="border border-gray-300 rounded-2xl w-96 py-2 px-4" type="text" />
+          <i class="absolute right-6 top-2.5"><SearchIcon class="w-5 h-5"/></i>
+        </div>
+        <div class="flex gap-4">
+          <div class="flex gap-2 items-center"><UserIcon class="w-5 h-5"/><a href="#">Conta</a></div>
+          <div class="flex gap-2 items-center"><ShoppingCartIcon class="w-5 h-5"/><a href="#">Carrinho</a></div>
+        </div>
+      </div>
+    </header>
+    </>
+  );
+}
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,4 +1,6 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
+import Header from "$store/components/header/Header.tsx";
+
 
 /** @title {{{title}}} - {{{href}}} */
 export interface Link {
@@ -11,43 +13,25 @@ export interface Props {
   title?: string;
   /** @format textarea */
   headline?: string;
-  links?: Array<Link>;
+  headsub?: string;
+  heroBg?: string;
 }
 
 export default function Hero({
   title = "deco.cx",
   logo = "/logo.svg",
-  headline =
-    "triadscripters",
-  links = [
-    { title: "Official website", "href": "https://deco.cx/" },
-    { title: "Linkedin", "href": "https://www.linkedin.com/company/deco-cx/" },
-    { title: "Discord", "href": "https://deco.cx/discord" },
-  ],
+  headline = "Shopping And Department Store.",
 }: Props) {
   return (
-    <header class="lg:container mx-8 md:mx-16 lg:mx-auto mt-8 md:mt-12 mb-28 text-xl md:text-base">
-      <div class="mb-10 md:mb-20">
-        <img
-          class="object-cover w-20"
-          src={logo}
-          alt={title}
-        />
-      </div>
-      <div class="font-bold text-3xl lg:text-6xl leading-tight lg:leading-none xl:w-5/6">
-        {headline}
-      </div>
-      {!!links?.length && (
-        <ul class="mt-8 flex flex-col md:flex-row gap-2 md:gap-4">
-          {links.map(({ href, title }) => (
-            <li>
-              <a target="_blank" href={href} aria-label={title} class="link">
-                {title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
-    </header>
+    <>
+    <Header logo={logo} />
+    <section class="w-100 bg-cover bg-no-repeat bg-center h-[700px] flex items-center" style="background-image: url('/summer/bg.jpg');">
+        <div class="lg:container mx-8 md:mx-16 lg:mx-auto text-gray-100">
+          <h1 class="text-6xl font-bold">Estilo que faz você <br/> se destacar</h1>
+          <p class="my-6 text-xl">Nossas roupas são projetadas não apenas para vestir, mas <br/> para empoderar você a se destacar em qualquer ocasião.</p>
+          <button class="rounded-2xl text-xl bg-primary font-semibold py-3 px-6">Saiba mais</button>
+        </div>
+    </section>
+    </>
   );
 }
